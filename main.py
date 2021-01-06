72244f01020919a2a70ea7ff902380e22270585f
howtus <howtus12@gmail.com>
2021-01-06 22:40:06 +0300

    Upgrade script

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import requests
 import shutil
 import time
@@ -7,11 +8,18 @@ url = 'https://thispersondoesnotexist.com/image'
 
 def get_image(index):
     response = requests.get(url, stream=True)
-    with open('img' + str(index) + '.jpg', 'wb') as file:
+    with open('image/img' + str(index) + '.jpg', 'wb') as file:
         shutil.copyfileobj(response.raw, file)
 
 
-for i in range(1, 101):
-    print(i)
-    get_image(i)
-    time.sleep(1)
+if __name__ == "__main__":
+    print("This python script will download images from thispersondoesnotexist.com")
+    print("Please, put indexes for images [a, b]")
+    print("They will save as images/img*.jpg where * = [a, b]")
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    Path('image').mkdir(parents=True, exist_ok=True)
+    for i in range(a, b + 1):
+        print(f"Downloading img{i}.jpg...")
+        get_image(i)
+        time.sleep(0.7)
